c81f95fb5844e13d7011fe8ae4a92325e438cd4e
rashida richmond <richmond1895@gmail.com>
2021-07-27 12:57:41 -0400

    spa day

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        // [HttpPost("/User/Add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+        [HttpPost("/User/Add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.username = newUser.Name;
+                return View("~/Views/User/Index.cshtml");
+            }
+
+
+            else
+
+            {
+
+                ViewBag.error = "Passwords do not match";
+
+                return View("~/Views/User/Add.cshtml");
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+
+        public User(string name, string email, string password)
+        {
+
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+
+
+        }
+    }
+}
